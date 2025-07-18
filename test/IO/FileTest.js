edb7716ae0577f6822a2e8bec2c8bdd464738f56
Cédric Belin <cedx@outlook.com>
2025-07-19 00:44:07 +0200

    Fix the tests

--- test/IO/FileTest.js
+++ test/IO/FileTest.js
@@ -11,7 +11,7 @@ describe("File utilities", () => {
 
 	describe("toDataUrl()", () => {
 		it("should convert the specified file to a data URL", async () => {
-			const base64 = `Q8OpZHJpYyBCZWxpbiA8Y2VkcmljQGJlbGluLmlv${platform == "win32" ? "Pg0K" : "Pgo="}`;
+			const base64 = `Q8OpZHJpYyBCZWxpbiA8Y2VkeEBvdXRsb29rLmNvbT4${platform == "win32" ? "NCg==" : "K"}`;
 			const url = await toDataUrl("Authors.txt", "text/plain");
 			equal(url.href, `data:text/plain;base64,${base64}`);
 		});
